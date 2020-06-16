bc7e6fc12e371e60e99519b5a3ce8dda2e342467
frankopkusianwar <okirorfrank3@gmail.com>
2020-06-16 12:47:46 +0300

    add following statistics to homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Following", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
 
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Following", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
 
   # Following a user.
