73bd2ee8673705d8e3fce48a04d0e8b83f1bbfdf
frankopkusianwar <okirorfrank3@gmail.com>
2020-06-18 18:30:31 +0300

    heroku setup

--- db/migrate/20200615130706_create_followings.rb
+++ db/migrate/20200615130706_create_followings.rb
@@ -6,9 +6,5 @@ class CreateFollowings < ActiveRecord::Migration[6.0]
 
       t.timestamps
     end
-    add_index :followings, :follower_id
-    add_index :followings, :followed_id
-    add_index :followings, [:follower_id, :followed_id], unique: true
-
   end
 end
--- db/migrate/20200615151252_change_column_names.rb
+++ db/migrate/20200615151252_change_column_names.rb
@@ -3,4 +3,7 @@ class ChangeColumnNames < ActiveRecord::Migration[6.0]
     rename_column :followings, :FollowerId, :follower_id
     rename_column :followings, :FollowedId, :followed_id
   end
+    add_index :followings, :follower_id
+    add_index :followings, :followed_id
+    add_index :followings, [:follower_id, :followed_id], unique: true
 end
