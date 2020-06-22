d008975493bfcc67b23db67d9088070fbbc12b52
frankopkusianwar <okirorfrank3@gmail.com>
2020-06-22 12:33:20 +0300

    add faker for fullname

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:fullname) { |n| "#{n}#{Faker::Internet.fullname}" }
+    sequence(:fullname) { |n| "#{n}#{Faker::Name.name}" }
     sequence(:username) { |n| "#{n}#{Faker::Internet.username}" }
   end
 end
